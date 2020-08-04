9d146c8da7606be41169080c877a43a014c95f6a
MrSharma619 <sh.manthan99@gmail.com>
2020-08-05 00:36:08 +0530

    final

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// Accordion
+
+var action = "click";
+var speed = "500"; 		 //(in ms)
+
+$(document).ready(function(){
+
+	// Question handler
+	$('li.q').on(action, function(){
+
+		// Get next element
+		$(this).next()
+			.slideToggle(speed)
+
+				// Select all other answers
+				.siblings('li.a')
+					.slideUp();
+
+	// Get image for active question
+	var img = $(this).children('img');
+
+	// Remove the 'rotate' class for all images except the active
+	$('img').not(img).removeClass('rotate');
+
+	// Toggle rotate class
+	img.toggleClass('rotate');
+
+	
+	});
+});
